ac5728c50f968fc6f1eaf6654cf7ba86405f0c05
pedro.stoi <pstoicescu@deloitte.pt>
2023-12-07 11:40:47 +0000

    update

--- blocks/footerBottom/footerBottom.js
+++ blocks/footerBottom/footerBottom.js
@@ -2,16 +2,16 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 /**
- * loads and decorates the footer
- * @param {Element} block The footer block element
+ * loads and decorates the footerBottom
+ * @param {Element} block The footerBottom block element
  */
 export default async function decorate(block) {
-  const footerMeta = getMetadata('footerBottom');
+  const footerBottomMeta = getMetadata('footerBottom');
   block.textContent = '';
 
   // load footer fragment
-  const footerPath = footerMeta.footer || '/footerBottom';
-  const fragment = await loadFragment(footerPath);
+  const footerBottomPath = footerBottomMeta.footerBottom || '/footerBottom';
+  const fragment = await loadFragment(footerBottomPath);
 
   // decorate footer DOM
   const footer = document.createElement('div');
