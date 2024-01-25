aca0d85223b888f48c063f2ee5d19fffb0e6baa8
Bernardo Almeida <bernalmeida@deloitte.pt>
2024-01-25 18:00:11 +0000

    arrows almost functional

--- blocks/slider/slider.js
+++ blocks/slider/slider.js
@@ -1,14 +1,31 @@
 const slider = document.querySelector('.slider');
 const sliderWrapper = document.querySelector('.slider-wrapper');
+const leftButton = document.querySelector('.arrows-wrapper .arrows > div:first-child');
+const rightButton = document.querySelector('.arrows-wrapper .arrows > div:nth-child(2)');
 
 let isDragging = false;
 let startPosition = 0;
 let currentTranslate = 0;
+let currentSlide = 0;
+let totalSlides = 0.65;
 
 slider.addEventListener('mousedown', (e) => startDragging(e));
 slider.addEventListener('mouseup', () => stopDragging());
 slider.addEventListener('mouseleave', () => stopDragging());
 slider.addEventListener('mousemove', (e) => drag(e));
+
+leftButton.addEventListener('click', function() {
+    console.log("left");
+    currentSlide = (currentSlide - 0.2 + totalSlides) % totalSlides;
+    updateSlider();
+  });
+
+  rightButton.addEventListener('click', function() {
+    console.log("rigth");
+    currentSlide = Math.min(currentSlide + 0.1, totalSlides - 1);
+    updateSlider();
+});
+
 
 function startDragging(e) {
     isDragging = true;
@@ -39,3 +56,9 @@ function drag(e) {
 function setSliderPosition() {
     slider.style.transform = `translateX(${currentTranslate}px)`;
 }
+
+function updateSlider() {
+    const translateValue = -currentSlide * sliderWrapper.offsetWidth + 'px';
+    slider.style.transform = 'translateX(' + translateValue + ')';
+}
+
