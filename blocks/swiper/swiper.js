71fd6d68a28bb198503383bef6c10802e68b91f7
Bernardo Almeida <bernalmeida@deloitte.pt>
2024-02-05 10:24:20 +0000

    scripts changed to head.html

--- blocks/swiper/swiper.js
+++ blocks/swiper/swiper.js
@@ -1,89 +1,68 @@
-// main.js
+var swiperWrapper = document.querySelector('.swiper-wrapper');
+var swiperBlock = document.querySelector('.swiper.block');
+var swiperDiv = document.querySelector('.swiper');
+var swiperButtonPrev = document.querySelector('.section > div:nth-child(3)');
+var swiperButtonNext = document.querySelector('.section > div:nth-child(4)');
 
-// Function to dynamically load CSS file
-function loadCSS(filename) {
-    var link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = filename;
-    document.head.appendChild(link);
+if (swiperWrapper) {
+    swiperWrapper.classList.remove('swiper-wrapper');
+    swiperWrapper.classList.add('swiper');
+} else {
+    console.log("Element with class 'swiper-wrapper' not found");
 }
 
-// Function to dynamically load JS file
-function loadJS(filename, callback) {
-    var script = document.createElement('script');
-    script.src = filename;
-    script.onload = callback;
-    document.body.appendChild(script);
+if (swiperBlock) {
+    swiperBlock.classList.remove('block');
+    swiperBlock.classList.remove('swiper');
+    swiperBlock.classList.add('swiper-wrapper');
+} else {
+    console.log("Element with class 'swiper block' not found");
 }
 
-loadCSS('https://unpkg.com/swiper/swiper-bundle.min.css');
-loadJS('https://unpkg.com/swiper/swiper-bundle.min.js', function () {
-    var swiperWrapper = document.querySelector('.swiper-wrapper');
-    var swiperBlock = document.querySelector('.swiper.block');
-    var swiperDiv = document.querySelector('.swiper');
-    var swiperButtonPrev = document.querySelector('.section > div:nth-child(3)');
-    var swiperButtonNext = document.querySelector('.section > div:nth-child(4)');
+if (swiperButtonPrev) {
+    swiperButtonPrev.classList.add('swiper-button-prev');
+} else {
+    console.log("Element not found");
+}
 
-    if (swiperWrapper) {
-        swiperWrapper.classList.remove('swiper-wrapper');
-        swiperWrapper.classList.add('swiper');
-    } else {
-        console.log("Element with class 'swiper-wrapper' not found");
-    }
-
-    if (swiperBlock) {
-        swiperBlock.classList.remove('block');
-        swiperBlock.classList.remove('swiper');
-        swiperBlock.classList.add('swiper-wrapper');
-    } else {
-        console.log("Element with class 'swiper block' not found");
-    }
-
-    if (swiperButtonPrev) {
-        swiperButtonPrev.classList.add('swiper-button-prev');
-    } else {
-        console.log("Element not found");
-    }
-
-    if (swiperButtonNext) {
-        swiperButtonNext.classList.add('swiper-button-next');
-    } else {
-        console.log("Element not found");
-    }
+if (swiperButtonNext) {
+    swiperButtonNext.classList.add('swiper-button-next');
+} else {
+    console.log("Element not found");
+}
 
 
-    if (swiperDiv) {
-        console.log("found");
-        var innermostDivs = swiperDiv.querySelectorAll('.swiper-wrapper > div');
+if (swiperDiv) {
+    console.log("found");
+    var innermostDivs = swiperDiv.querySelectorAll('.swiper-wrapper > div');
 
-        innermostDivs.forEach(function (div) {
-            div.classList.add('swiper-slide');
-        });
-    } else {
-        console.log("not found");
-    }
-
-    var swiper = new Swiper('.swiper', {
-        direction: 'horizontal',
-        spaceBetween: 30,
-        
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-
-          breakpoints: {
-            0: {
-                slidesPerView: 2,
-            },
-            700: {
-                slidesPerView: 2.5,
-            },
-            970:{
-                slidesPerView: 3.5,
-            },
-        },
-        
-          
+    innermostDivs.forEach(function (div) {
+        div.classList.add('swiper-slide');
     });
+} else {
+    console.log("not found");
+}
+
+var swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 2,
+        },
+        700: {
+            slidesPerView: 2.5,
+        },
+        970: {
+            slidesPerView: 3.5,
+        },
+    },
+
+
 });
